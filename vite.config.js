45dba91bc24b28ee1df3d56cf987c974814a9ca3
Kelvin <dcha0120@student.monash.edu>
2025-10-24 18:13:22 +1100

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FIT5032-week12/',
   plugins: [
     vue(),
     vueDevTools(),
